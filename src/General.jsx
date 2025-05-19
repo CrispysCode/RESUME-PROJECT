ae3c696b435c56e93dff3738546f0949ed06fe78
CrispysCode <pr0ject0dinx@gmail.com>
2025-05-18 22:01:19 -0500

    first commit

---
+++ src/General.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function Name() {
+    
+    return <input></input>
+}
+
+function Email() {
+    return <input></input>
+}
+
+
+function Phone() {
+    return <input></input>
+}
+
+export default function General() {
+
+  return (
+    <div>
+    <Name />
+    <Email />
+    <Phone />
+    </div>
+  );
+}
+
+
